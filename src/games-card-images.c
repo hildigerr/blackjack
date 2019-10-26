3ed980e513c7b366127df9f6c1d6df9497586061
Hildigerr Vergaray <Roberto.Vergaray@YmirSystems.com>
2019-10-26 16:19:56 -0700

    gnome-games 2.7.1

---
+++ src/games-card-images.c
@@ -0,0 +1,177 @@
+/* games-card-images.c
+   Copyright 2004 Callum McKenzie
+
+   This library is free software; you can redistribute it and'or modify
+   it under the terms of the GNU Library General Public License as published 
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* Authors:   Callum McKenzie <callum@physics.otago.ac.nz> */
+
+/* Manage a set of pixbufs containing a deck of cards. */
+
+#include <glib.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
+
+#include "games-card-images.h"
+
+#include "games-card-common.h"
+
+G_DEFINE_TYPE(GamesCardImages, games_card_images, G_TYPE_OBJECT);
+
+GamesCardImages * games_card_images_new (void)
+{
+  return g_object_new (GAMES_TYPE_CARD_IMAGES, NULL);
+}
+
+static void games_card_images_render (GamesCardImages * images, gint cardid)
+{
+  int sx, sy;
+  GdkPixbuf * subpixbuf;
+
+  sx = cardid % 13;
+  sy = cardid / 13;
+
+  subpixbuf = gdk_pixbuf_new_subpixbuf (images->source, sx*images->subwidth, 
+					sy*images->subheight,
+					images->subwidth, images->subheight);
+  images->pixbufs[cardid] = gdk_pixbuf_scale_simple (subpixbuf, images->width, 
+						     images->height, 
+						     GDK_INTERP_BILINEAR);
+  images->rendered[cardid] = TRUE;
+  g_object_unref (subpixbuf);
+}
+
+static void games_card_images_prerender (GamesCardImages * images)
+{
+  gchar * fullname;
+
+  /* FIXME: We should search a path. */
+  fullname = g_strconcat (CARDDIR, images->themename, NULL);
+  images->source = gdk_pixbuf_new_from_file (fullname, NULL);
+  g_free (fullname);
+
+  if (!images->source) {
+    g_warning ("Using a fallback card image set.");
+    fullname = CARDDIR"bonded.png";
+    images->source = gdk_pixbuf_new_from_file (fullname, NULL);    
+  }
+
+  if (!images->source) {
+    /* FIXME: Find a better way to report errors. */
+    g_warning ("Failed to load the fallback file.\n");
+  }
+
+  images->subwidth = gdk_pixbuf_get_width (images->source)/13;
+  images->subheight = gdk_pixbuf_get_height (images->source)/5;
+
+  images->prerendered = TRUE;
+}
+
+static void games_card_images_purge (GamesCardImages * images)
+{
+  int i;
+
+  if (images->prerendered)
+    g_object_unref (images->source);
+
+  for (i=0; i<GAMES_CARDS_TOTAL; i++) {
+    if (images->rendered[i])
+      g_object_unref (images->pixbufs[i]);
+    images->rendered[i] = FALSE;
+  }
+  
+  images->prerendered = FALSE;
+}
+
+GdkPixbuf * games_card_images_get_card_by_id (GamesCardImages * images,
+					      gint cardid)
+{
+  if (!images->prerendered)
+    games_card_images_prerender (images);
+
+  if (!images->rendered[cardid])
+    games_card_images_render (images, cardid);
+
+  return images->pixbufs[cardid];
+}
+
+GdkPixbuf * games_card_images_get_card (GamesCardImages * images, gint suit,
+					gint rank)
+{
+  return games_card_images_get_card_by_id (images, GAMES_CARD_ID(suit, rank));
+}
+
+GdkPixbuf * games_card_images_get_back (GamesCardImages * images)
+{
+  return games_card_images_get_card_by_id (images, GAMES_CARD_BACK);
+}
+
+GdkPixbuf * games_card_images_get_red_joker (GamesCardImages * images)
+{
+  return games_card_images_get_card_by_id (images, GAMES_CARD_RED_JOKER);
+}
+
+GdkPixbuf * games_card_images_get_black_joker (GamesCardImages * images)
+{
+  return games_card_images_get_card_by_id (images, GAMES_CARD_BLACK_JOKER);
+}
+
+void games_card_images_set_size (GamesCardImages * images, 
+				 gint width, gint height)
+{
+  if ((width == images->width) && (height == images->height))
+    return;
+
+  images->width = width;
+  images->height = height;
+
+  games_card_images_purge (images);
+}
+
+void games_card_images_set_theme (GamesCardImages * images, gchar * name)
+{
+  /* Ignore the two obvious problem cases silently. */
+  if ((!name) || (*name == '\0'))
+    return;
+
+  if (images->themename)
+    g_free (images->themename);
+
+  images->themename = g_strdup (name);
+
+  games_card_images_purge (images);
+}
+
+static void games_card_images_finalize (GamesCardImages * images)
+{
+  games_card_images_purge (images);
+}
+
+static void games_card_images_class_init (GamesCardImagesClass *class)
+{
+  GObjectClass *oclass = G_OBJECT_CLASS (class);
+
+  oclass->finalize = (GObjectFinalizeFunc) games_card_images_finalize;
+}
+
+static void games_card_images_init (GamesCardImages *cardimages)
+{
+  /* This is the size of the original gdk-card-image cards. */
+  cardimages->width = 79;
+  cardimages->height = 123;
+  cardimages->themename = g_strdup ("bonded-new.png");
+
+  cardimages->prerendered = FALSE;
+  cardimages->rendered = g_new0 (gboolean, GAMES_CARDS_TOTAL);
+  cardimages->pixbufs = g_new0 (GdkPixbuf *, GAMES_CARDS_TOTAL); 
+}
---
+++ src/games-card-images.h
@@ -0,0 +1,103 @@
+/* games-card-images.h
+   Copyright 2004 Callum McKenzie
+
+   This library is free software; you can redistribute it and'or modify
+   it under the terms of the GNU Library General Public License as published 
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* Authors:   Callum McKenzie <callum@physics.otago.ac.nz> */
+
+/* Manage a set of pixbufs containing a deck of cards. */
+
+#ifndef GAMES_CARD_IMAGES_H
+#define GAMES_CARD_IMAGES_H
+
+G_BEGIN_DECLS
+
+#include <glib.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
+
+enum {
+  GAMES_CARD_ACE         = 1,
+  GAMES_CARD_TWO         = 2,
+  GAMES_CARD_THREE       = 3,
+  GAMES_CARD_FOUR        = 4,
+  GAMES_CARD_FIVE        = 5,
+  GAMES_CARD_SIX         = 6,
+  GAMES_CARD_SEVEN       = 7,
+  GAMES_CARD_EIGHT       = 8,
+  GAMES_CARD_NINE        = 9,
+  GAMES_CARD_TEN         = 10,
+  GAMES_CARD_JACK        = 11,
+  GAMES_CARD_QUEEN       = 12,
+  GAMES_CARD_KING        = 13,
+  GAMES_CARDS_CLUBS      = 0,
+  GAMES_CARDS_HEARTS     = 1,
+  GAMES_CARDS_DIAMONDS   = 2,
+  GAMES_CARDS_SPADES     = 3,
+  GAMES_CARD_RED_JOKER   = 52,
+  GAMES_CARD_BLACK_JOKER = 53,
+  GAMES_CARD_BACK        = 54,
+  GAMES_CARDS_TOTAL       = 55
+};
+
+typedef struct _GamesCardImages {
+  GObject parent;
+
+  gint width;
+  gint height;
+
+  gchar * themename;
+
+  GdkPixbuf * source;
+  gint subwidth, subheight;
+  gboolean prerendered;
+  gboolean *rendered;
+  GdkPixbuf ** pixbufs;
+} GamesCardImages;
+
+typedef struct _GamesCardImagesClass {
+  GObjectClass parent;
+} GamesCardImagesClass;
+
+#define GAMES_TYPE_CARD_IMAGES (games_card_images_get_type ())
+#define GAMES_CARD_IMAGES(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), GAMES_TYPE_CARD_IMAGES, GamesCardImages))
+#define GAMES_CARD_IMAGES_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), GAMES_TYPE_CARD_IMAGES, GamesCardImagesClass))
+#define GAMES_IS_CARD_IMAGES(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GAMES_TYPE_CARD_IMAGES))
+#define GAMES_IS_CARD_IMAGES_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GAMES_TYPE_CARD_IMAGES))
+#define GAMES_CARD_IMAGES_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), GAMES_TYPE_CARD_IMAGES))
+
+GType games_card_images_get_type (void);
+
+GamesCardImages * games_card_images_new (void);
+
+/* The real card routine. */
+GdkPixbuf * games_card_images_get_card_by_id (GamesCardImages * images,
+					      gint cardid);
+
+/* The convenience functions. */
+#define GAMES_CARD_ID(suit, rank) ((13*(suit)) + (rank-1))
+
+GdkPixbuf * games_card_images_get_card (GamesCardImages * images, gint suit, 
+					gint rank);
+GdkPixbuf * games_card_images_get_red_joker (GamesCardImages * images);
+GdkPixbuf * games_card_images_get_black_joker (GamesCardImages * images);
+GdkPixbuf * games_card_images_get_back (GamesCardImages * images);
+
+void games_card_images_set_size (GamesCardImages * images, 
+				 gint width, gint height);
+void games_card_images_set_theme (GamesCardImages * images, gchar * name);
+
+G_END_DECLS
+
+#endif /* GAMES_CARD_IMAGES_H */
